1133b41e5fd97364ea6af9d2987f4975902fe3f4
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-07-17 20:31:33 +0600

    file extention list create

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import os
 import sys
+import shutil
 
-def files_list(path):
-    files=os.listdir(path)
-    print(files)
+
+def list_file(path):
+    files_list=os.listdir(path)
+    extention_list=[]
+    
+    for exe in files_list:
+        if exe.split('.')[1] not in extention_list:
+            extention_list.append(exe.split('.')[1])
+            
+    print(files_list)
+    print (extention_list)
+    
+
+
+
+
+# def files_list(path):
+#     files_list=os.listdir(path)
+    
+#     jpg_files=[jpg for jpg in files_list if jpg.split(".")[1] == 'jpg']
+#     png_files=[png for png in files_list if png.split('.')[1] == 'png']
+#     print(jpg_files)
+#     print(png_files)
     
 
 def main():
     line = sys.argv[1]
     print(line,type(line))
-    files_list(line)
+    # files_list(line)
+    list_file(line)
 
 if __name__ =="__main__":
     main()
